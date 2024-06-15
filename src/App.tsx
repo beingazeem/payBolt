0a0f0731be6da39dd74f7451810ba86b756dd88b
Rahul <srayesh5432@gmail.com>
2024-06-15 16:41:18 +0530

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div className='text-center text-emerald-300'>Coming Soon</div>
+    </>
+  )
+}
+
+export default App
