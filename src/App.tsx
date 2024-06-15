4ab4214d2cb83ea8bc19bdf758d6aff5a679a7bd
Vivek Kumar <vivekartist9999@gmail.com>
2024-06-15 20:28:38 +0530

    feat: add header & hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-function App() {
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Home } from './modules/Home';
 
+import './App.css';
+
+function App() {
   return (
     <>
-      <div className='text-center text-emerald-300'>Coming Soon</div>
+      <Header />
+      <main className="pt-32">
+        <Routes>
+          <Route>
+            <Route index path="/" element={<Home />} />
+            <Route index path="/products" element={<div>Products</div>} />
+            <Route index path="/about" element={<div>About</div>} />
+            <Route index path="/pricing" element={<div>Pricing</div>} />
+            <Route index path="/login" element={<div>Login</div>} />
+            <Route index path="/signup" element={<div>Sign up</div>} />
+            <Route index path="*" element={<div>404</div>} />
+          </Route>
+        </Routes>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        foreground: `hsl(var(--foreground))`,
+        background: `hsl(var(--background))`,
+        primary: `hsl(var(--primary))`,
+      },
+      zIndex: {
+        header: 100,
+      },
+    },
   },
   plugins: [],
-}
-
+};
