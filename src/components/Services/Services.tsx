0e4a0bde11c29342aed8701fda96186f89a9b18f
Vivek Kumar <vivekartist9999@gmail.com>
2024-06-16 20:36:35 +0530

    fix: service section responsive

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@shared/utils';
-import { ServicesConstants } from '../../constants/Services.constants';
+import { ServicesConstants } from '@/constants/Services.constants';
+
 const Services = () => {
   return (
     <div className="mx-2 my-2 px-2 py-2 flex flex-col gap-20">
@@ -19,28 +20,31 @@ const Services = () => {
               <div className="flex flex-col justify-center items-start gap-5 order-2 lg:order-1">
                 <h3 className="text-5xl font-bold w-full">
                   <span
-                    className={cn('block', {
-                      'text-end': services.isImageLeft,
+                    className={cn('block text-center', {
+                      'lg:text-end': services.isImageLeft,
                     })}
                   >
                     {services.heading}
                   </span>{' '}
                   <span
-                    className={cn('block', {
-                      'text-end': services.isImageLeft,
+                    className={cn('block text-center', {
+                      'lg:text-end': services.isImageLeft,
                     })}
                   >
                     Amplifying Success.
                   </span>
                 </h3>
                 <p
-                  className={cn('text-foreground/70 max-w-xl w-full', {
-                    'text-end': services.isImageLeft,
-                  })}
+                  className={cn(
+                    'text-foreground/70 max-w-xl w-full text-center',
+                    {
+                      'lg:text-end': services.isImageLeft,
+                    },
+                  )}
                 >
                   {services.desc}
                 </p>
-                <div className="flex gap-x-5 items-center flex-wrap justify-end w-full">
+                <div className="flex gap-x-5 items-center flex-wrap justify-center lg:justify-end w-full">
                   <button className="py-2 px-8 mt-5 rounded-full border-foreground/30 transition-colors border font-bold hover:bg-foreground hover:text-background">
                     Learn More
                   </button>
@@ -50,12 +54,18 @@ const Services = () => {
           ) : (
             <>
               <div className="flex flex-col justify-center items-start gap-5 order-2 lg:order-1">
-                <h3 className="text-5xl font-bold">
-                  <span className="block">{services.heading}</span>{' '}
-                  <span className="block">Amplifying Success.</span>
+                <h3 className="text-5xl font-bold w-full">
+                  <span className="block w-full text-center lg:text-start">
+                    {services.heading}
+                  </span>{' '}
+                  <span className="block w-full text-center lg:text-start">
+                    Amplifying Success.
+                  </span>
                 </h3>
-                <p className="text-foreground/70 max-w-xl">{services.desc}</p>
-                <div className="flex gap-x-5 items-center flex-wrap">
+                <p className="text-foreground/70 lg:max-w-xl text-center lg:text-start w-full">
+                  {services.desc}
+                </p>
+                <div className="flex gap-x-5 items-center justify-center lg:justify-start flex-wrap w-full">
                   <button className="py-2 px-8 mt-5 rounded-full border-foreground/30 transition-colors border font-bold hover:bg-foreground hover:text-background">
                     Learn More
                   </button>
