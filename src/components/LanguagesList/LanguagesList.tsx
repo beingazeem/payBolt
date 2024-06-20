02f5f18522507787937cb807aafe5556937e7874
Rahul <srayesh5432@gmail.com>
2024-06-20 21:58:21 +0530

    fix responsivness

--- src/components/LanguagesList/LanguagesList.tsx
+++ src/components/LanguagesList/LanguagesList.tsx
@@ -6,7 +6,7 @@ const LanguagesList = () => {
     <section className="py-6 cardTheme">
       <div className="px-2 font-bold flex items-center justify-center gap-x-2 ">
         <span className="text-5xl font-bold relative text-center -translate-x-14">
-          <span>Build for </span>
+          <span>Built for </span>
           <ReactTyped
             strings={TranslationsLang}
             typeSpeed={80}
